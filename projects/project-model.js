b95df46f4e647d4373e8a97244fb904d1c129a08
William <billy.ryan.rom@gmail.com>
2020-03-20 12:25:37 -0400

    Add db methods inside of project-model.js

---
+++ projects/project-model.js
@@ -0,0 +1,39 @@
+const db = require('../data/knex-config')
+
+module.exports = {
+    addRescs,
+    findRescs,
+    addPros,
+    findPros,
+    addTasks,
+    findTasks
+}
+
+function addRescs(data){
+    return db('resources')
+    .insert(data)
+}
+
+function findRescs(){
+    return db('resources')
+}
+
+function addPros(data){
+    return db('projects')
+    .insert(data)
+}
+
+function findPros(){
+    return db('projects')
+}
+
+function addTasks(data){
+    return db('tasks')
+    .insert(data)
+}
+
+function findTasks(){
+    return db('tasks as t')
+    .select('t.*', 'p.project_name','p.project_description')
+    .join('projects as p', 't.project_id', '=', 'p.id')
+}
